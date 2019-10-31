d9ca0a86925799520c7d63485586ee8e26296883
Larry Kridner <lkk180001@utdallas.edu>
2019-10-31 09:56:25 -0500

    Just need to work on rehash method, also need to check on why the hashtable doesn't enter into 0

--- src/HashTableLinearProbe.java
+++ src/HashTableLinearProbe.java
@@ -11,26 +11,133 @@ public class HashTableLinearProbe <K, V>{
 			deleted = false;
 		}
 	}
-	
-	HashEntry <K, V>[] hashtable = new HashEntry[10];
+	public int tableSize = 7;
+	HashEntry <K, V>[] hashtable;
+	public HashTableLinearProbe() {
+		hashtable = new HashEntry[tableSize];
+	}
 	
 	public boolean insert (K key, V value) {
-		return true;
+		boolean status = false;
+		int count = 0; // to know how big the array is
+		int hashValue = getHashValue(key);
+		
+		for (int i = hashValue; i < tableSize; i++) {
+			if (hashtable[i]== null) {
+				hashtable[i] = new HashEntry(key, value);
+				status = true;
+				return status;
+			}
+			else if(i == tableSize-1) {
+				i = 0;
+			}
+			else if (count == tableSize) { //Stops if we have traversed the whole tree once
+				rehash();
+			}
+			else {
+				count++;
+			}
+		}
+		return status;
 	}
 	
 	public V find (K key){
-		return null;
+		V temp = null;
+		int count = 0;
+		int hashValue = getHashValue(key);
+		
+		for (int i = hashValue; i < tableSize; i++) {
+			if (key == hashtable[i].inputkey) {
+				temp = hashtable[i].inputvalue;
+				return temp;
+			}
+			else if (i == tableSize-1) {
+				i = 0;
+			}
+			else if (count == tableSize - 1) { //Stops if we have traversed the whole tree once
+				break;
+			}
+			else if (count < hashValue && hashtable[i].inputkey == null) { //Checks for if there is a space 
+				break;
+			}
+			else {
+				count++;
+			}
+		}
+		return temp;
 	}
 	
 	public boolean delete (K key) {
-		return true;
+		boolean status = false;
+		int hashValue = getHashValue(key);
+		int count = 0;
+		for (int i = hashValue; i < tableSize; i++) {
+				if (hashtable[i] != null && key.equals(hashtable[i].inputkey)) {
+				hashtable[i].deleted = true;
+				status = true;
+				return status;
+			}
+			else if (i == tableSize-1) {
+				i = 0;
+			}
+			else if (count == tableSize - 1) { //Stops if we have traversed the whole tree once
+				break;
+			}
+			else if (hashtable[i] == null) { //Checks for if there is a space 
+				break;
+			}
+			else {
+				System.out.println(hashtable[i].inputkey);
+				count++;
+			}
+		}
+		return status;
 	}
 	
 	public int getHashValue(K key) {
-		return 0;
+		int hashValue = 0;
+		int totalValue = 0;
+		String keyString = (String) key; // need to parse to string
+		for (int i = 0; i < keyString.length(); i++) {
+			if (keyString.charAt(i) < 58) {
+				totalValue = Integer.valueOf(keyString);
+				break;
+			}
+			else {
+				totalValue += keyString.charAt(i);
+			}
+		}
+		hashValue = totalValue % tableSize;
+
+		return hashValue;
 	}
 	
 	public void rehash() {
+		//temp solution to increase hashtable by a prime number
+		int primeNumbers [] = {17,37,79};
+		int oldTableSize = tableSize;
+		for (int i = 0; i < primeNumbers.length; i++) {
+			if (tableSize < primeNumbers[i]) {
+				tableSize = primeNumbers[i];
+				break;
+			}
+		}
+		HashEntry<K, V> [] newTable = new HashEntry[tableSize];
 		
+		for (int i = 0; i < oldTableSize; i++) {
+			if(hashtable[i] != null) {
+				int hashValue = getHashValue(hashtable[i].inputkey);
+				
+				for (int j = hashValue; j < tableSize; j++) {
+					if (newTable[j]== null) {
+						newTable[j] = new HashEntry(hashtable[i].inputkey, hashtable[i].inputvalue);
+					}
+					else if(j == tableSize-1) {
+						j = 0;
+					}
+				}
+			}
+		}
+		hashtable = newTable;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,89 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class Main {
 	public static void main(String args[]) {
+		HashTableLinearProbe<String, String> hashTable = new HashTableLinearProbe<String, String>();
+		Scanner input = new Scanner(System.in);
+		String userInput = "";
+		String key, value;
+		while (true) {
+			System.out.println(" 1: insert"
+					+ "\n 2: delete"
+					+ "\n 3: find"
+					+ "\n 4: test hashing"
+					+ "\n 5: end"
+					+ "\nWhat do you want to run?");
+			userInput = input.nextLine();
+			if (userInput.equals("insert") || userInput.equals("1")) {
+				System.out.println("input key");
+				userInput = input.nextLine();
+				key = userInput;
+				
+				System.out.println("input value");
+				userInput = input.nextLine();
+				value = userInput;
+				hashTable.insert(key, value);
+			}
+			else if(userInput.equals("delete") || userInput.equals("2")) {
+				System.out.println("input key");
+				userInput = input.nextLine();
+				key = userInput;
+				hashTable.delete(key);
+			}
+			else if (userInput.equals("find") || userInput.equals("3")) {
+				
+			}
+			else if (userInput.equals("test hashing") || userInput.equals("4")) {
+				
+			}
+			else if (userInput.equals("end") || userInput.contentEquals("5")) {
+				break;
+			}
+		}
 		
+		
+		/*Scanner in;
+		if (args.length != 2) {
+			System.out.print("Error Incorrect Arguments:" + Arrays.toString(args));
+			System.exit(0);
+		}
+		try {
+			File input_file = new File (args[0]);
+			in = new Scanner(input_file);
+			File output_file = new File (args[1]);
+			PrintWriter out;
+			out = new PrintWriter(output_file);
+			
+			HashTableLinearProbe hashTable = new HashTableLinearProbe();
+			
+			String operation = "";
+			String stringKey;
+			int intKey, value;
+			boolean result;
+			
+			while(in.hasNext()) {
+				operation = in.next();
+				
+				switch (operation) {
+				case "Insert" :
+					try {
+						//Need to figure out how to convert int and string
+					}
+					catch (Exception e) {
+						
+					}
+				}
+			}
+			
+			in.close();
+			out.close();
+		}
+		catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+		*/
 	}
+	
 }
